88c10260666d1f80b70f27cceb9b4e08fb3bcc6d
gWX1231951 <goutong1@h-partners.com>
2023-06-08 10:06:37 +0800

    添加fuzz用例
    Signed-off-by: gWX1231951 <goutong1@h-partners.com>

---
+++ test/fuzztest/data.h
@@ -0,0 +1,63 @@
+#ifndef TEST_FUZZTEST_GETDATA_H
+#define TEST_FUZZTEST_GETDATA_H
+
+/*
+ * Copyright (C) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <cstdint>
+
+#include "../../common/log.h"
+#include "securec.h"
+
+namespace OHOS {
+namespace NeuralNetworkRuntime {
+class Data {
+public:
+    Data(const uint8_t* data, size_t size)
+    {
+        dataFuzz = data;
+        dataSize = size;
+    }
+    template<class T> T GetData()
+    {
+        T object {};
+        size_t objectSize = sizeof(object);
+        if (dataFuzz == nullptr || objectSize > dataSize - dataPos) {
+            LOGE("[GetData]Data is not enough.");
+            return {};
+        }
+        if (memcpy_s(&object, objectSize, dataFuzz + dataPos, objectSize) != EOK) {
+            LOGE("[GetData]Memcpy_s failed.");
+            return {};
+        }
+        dataPos = dataPos + objectSize;
+        return object;
+    }
+    const uint8_t* getNowData()
+    {
+        return dataFuzz + dataPos;
+    }
+    size_t getNowDataSize()
+    {
+        return dataSize - dataPos;
+    }
+private:
+    const uint8_t* dataFuzz = nullptr;
+    size_t dataSize = 0;
+    size_t dataPos = 0;
+};
+} // namespace NeuralNetworkRuntime
+} // namespace OHOS
+
+#endif
