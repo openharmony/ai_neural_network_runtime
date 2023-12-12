f094b67a1381dd2e4475c36e6111b3afb9fbb365
maoyong <maoyong5@huawei.com>
2023-12-12 17:28:45 +0800

    fix codecheck warning
    Signed-off-by: maoyong <maoyong5@huawei.com>

--- frameworks/native/nnbackend.h
+++ frameworks/native/nnbackend.h
@@ -1,19 +1,35 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef NEURAL_NETWORK_RUNTIME_NNBACKEND_H
+#define NEURAL_NETWORK_RUNTIME_NNBACKEND_H
+
 #include "backend.h"
+
 #include "executor.h"
 #include "tensor.h"
 #include "tensor_desc.h"
 #include "device.h"
 #include "nncompiler.h"
 
-#ifndef NEURAL_NETWORK_RUNTIME_NNBACKEND_H
-#define NEURAL_NETWORK_RUNTIME_NNBACKEND_H
-
 namespace OHOS {
 namespace NeuralNetworkRuntime {
 class NNBackend : public Backend {
 public:
     explicit NNBackend(const std::shared_ptr<Device>& device, size_t backendID);
-    ~NNBackend();
+    ~NNBackend() override;
 
     // Backend Info
     size_t GetBackendID() const override;
--- frameworks/native/nncompiler.h
+++ frameworks/native/nncompiler.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2023 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef NEURAL_NETWORK_RUNTIME_NNCOMPILER_H
+#define NEURAL_NETWORK_RUNTIME_NNCOMPILER_H
+
 #include "compiler.h"
 
 #include "mindir.h"
@@ -5,9 +23,6 @@
 #include "inner_model.h"
 #include "prepared_model.h"
 #include "nnexecutor.h"
-
-#ifndef NEURAL_NETWORK_RUNTIME_NNCOMPILER_H
-#define NEURAL_NETWORK_RUNTIME_NNCOMPILER_H
 
 namespace OHOS {
 namespace NeuralNetworkRuntime {
@@ -17,7 +32,7 @@ public:
     NNCompiler() = delete;
     NNCompiler(std::shared_ptr<Device> device, size_t backendID);
     NNCompiler(const void* model, std::shared_ptr<Device> device, size_t backendID);
-    ~NNCompiler();
+    ~NNCompiler() override;
 
     size_t GetBackendID() const override;
 
