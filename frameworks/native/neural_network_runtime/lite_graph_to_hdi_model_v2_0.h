7230983af4ec375860679551ec73f847b65844c9
wangchuanxia <wangchuanxia@huawei.com>
2024-01-30 10:12:01 +0800

    mindir中litegraph转hdimodel挪到nnrt
    Signed-off-by: wangchuanxia <wangchuanxia@huawei.com>

---
+++ frameworks/native/neural_network_runtime/lite_graph_to_hdi_model_v2_0.h
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef NEURAL_NETWORK_RUNTIME_LITEGRAPH_TO_HDIMODEL_V2_0_H
+#define NEURAL_NETWORK_RUNTIME_LITEGRAPH_TO_HDIMODEL_V2_0_H
+
+#include "mindir.h"
+#include "nnrt/v2_0/model_types.h"
+
+namespace OHOS {
+namespace NeuralNetworkRuntime {
+namespace V2 {
+void HDIModel_Destroy(OHOS::HDI::Nnrt::V2_0::Model **model);
+OHOS::HDI::Nnrt::V2_0::Model *LiteGraph_To_HDIModel(const mindspore::lite::LiteGraph *lite_graph,
+    const OHOS::HDI::Nnrt::V2_0::SharedBuffer &buffer);
+} // V2
+} // NeuralNetworkRuntime
+} // OHOS
+
+#endif // NEURAL_NETWORK_RUNTIME_LITEGRAPH_TO_HDIMODEL_V2_0_H
