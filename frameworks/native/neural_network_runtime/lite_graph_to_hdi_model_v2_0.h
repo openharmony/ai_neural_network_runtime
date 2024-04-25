106eaef291eac18dec7dfeade604f5545c81d965
wang-yangsong <wangyangsong@h-partners.com>
2024-04-25 14:55:40 +0800

    fix code format
    Signed-off-by: wang-yangsong <wangyangsong@h-partners.com>

--- frameworks/native/neural_network_runtime/lite_graph_to_hdi_model_v2_0.h
+++ frameworks/native/neural_network_runtime/lite_graph_to_hdi_model_v2_0.h
@@ -23,7 +23,7 @@ namespace OHOS {
 namespace NeuralNetworkRuntime {
 namespace V2 {
 void HDIModel_Destroy(OHOS::HDI::Nnrt::V2_0::Model **model);
-OHOS::HDI::Nnrt::V2_0::Model *LiteGraph_To_HDIModel(const mindspore::lite::LiteGraph *lite_graph,
+OHOS::HDI::Nnrt::V2_0::Model *LiteGraph_To_HDIModel(const mindspore::lite::LiteGraph *liteGraph,
     const OHOS::HDI::Nnrt::V2_0::SharedBuffer &buffer);
 } // V2
 } // NeuralNetworkRuntime
