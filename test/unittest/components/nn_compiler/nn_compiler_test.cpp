96db894e6eb15b7245ea44024d1e9011c03f98ff
周翔 <zhouxiang78@h-partners.com>
2024-06-17 19:18:18 +0800

    UT用例新增
    Signed-off-by: 周翔 <zhouxiang78@h-partners.com>

---
+++ test/unittest/components/nn_compiler/nn_compiler_test.cpp
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2022 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "nnbackend.h"
+#include "nncompiler.h"
+#include "device.h"
+#include "interfaces/kits/c/neural_network_runtime/neural_network_runtime_type.h"
+#include "common/utils.h"
+
+using namespace testing;
+using namespace testing::ext;
+using namespace OHOS::NeuralNetworkRuntime;
+
+namespace OHOS {
+namespace NeuralNetworkRuntime {
+namespace UnitTest {
+class NNCompilerTest : public testing::Test {
+public:
+    NNCompilerTest() = default;
+    ~NNCompilerTest() = default;
+};
+
+class MockIDevice : public Device {
+public:
+    MOCK_METHOD1(GetDeviceName, OH_NN_ReturnCode(std::string&));
+    MOCK_METHOD1(GetVendorName, OH_NN_ReturnCode(std::string&));
+    MOCK_METHOD1(GetVersion, OH_NN_ReturnCode(std::string&));
+    MOCK_METHOD1(GetDeviceType, OH_NN_ReturnCode(OH_NN_DeviceType&));
+    MOCK_METHOD1(GetDeviceStatus, OH_NN_ReturnCode(DeviceStatus&));
+    MOCK_METHOD2(GetSupportedOperation, OH_NN_ReturnCode(std::shared_ptr<const mindspore::lite::LiteGraph>,
+        std::vector<bool>&));
+    MOCK_METHOD1(IsFloat16PrecisionSupported, OH_NN_ReturnCode(bool&));
+    MOCK_METHOD1(IsPerformanceModeSupported, OH_NN_ReturnCode(bool&));
+    MOCK_METHOD1(IsPrioritySupported, OH_NN_ReturnCode(bool&));
+    MOCK_METHOD1(IsDynamicInputSupported, OH_NN_ReturnCode(bool&));
+    MOCK_METHOD1(IsModelCacheSupported, OH_NN_ReturnCode(bool&));
+    MOCK_METHOD4(PrepareModel, OH_NN_ReturnCode(std::shared_ptr<const mindspore::lite::LiteGraph>,
+                                          const Buffer&,
+                                          const ModelConfig&,
+                                          std::shared_ptr<PreparedModel>&));
+    MOCK_METHOD4(PrepareModel, OH_NN_ReturnCode(const void*,
+                                          const Buffer&,
+                                          const ModelConfig&,
+                                          std::shared_ptr<PreparedModel>&));
+    MOCK_METHOD3(PrepareModelFromModelCache, OH_NN_ReturnCode(const std::vector<Buffer>&,
+                                                        const ModelConfig&,
+                                                        std::shared_ptr<PreparedModel>&));
+    MOCK_METHOD3(PrepareOfflineModel, OH_NN_ReturnCode(std::shared_ptr<const mindspore::lite::LiteGraph>,
+                                                 const ModelConfig&,
+                                                 std::shared_ptr<PreparedModel>&));
+    MOCK_METHOD1(AllocateBuffer, void*(size_t));
+    MOCK_METHOD2(AllocateTensorBuffer, void*(size_t, std::shared_ptr<TensorDesc>));
+    MOCK_METHOD2(AllocateTensorBuffer, void*(size_t, std::shared_ptr<NNTensor>));
+    MOCK_METHOD1(ReleaseBuffer, OH_NN_ReturnCode(const void*));
+    MOCK_METHOD2(AllocateBuffer, OH_NN_ReturnCode(size_t, int&));
+    MOCK_METHOD2(ReleaseBuffer, OH_NN_ReturnCode(int, size_t));
+};
+
+/**
+ * @tc.name: nncompilertest_construct_001
+ * @tc.desc: Verify the QuantParams function return nullptr in case of fd -1.
+ * @tc.type: FUNC
+ */
+HWTEST_F(NNCompilerTest, nncompilertest_construct_001, TestSize.Level0)
+{
+    size_t backendID = 1;
+    std::shared_ptr<MockIDevice> device = std::make_shared<MockIDevice>();
+
+    NNCompiler* nncompiler = new (std::nothrow) NNCompiler(device, backendID);
+    EXPECT_NE(nullptr, nncompiler);
+}
+} // namespace UnitTest
+} // namespace NeuralNetworkRuntime
+} // namespace OHOS
