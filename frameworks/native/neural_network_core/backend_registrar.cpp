44e53b5d88ad988a45be3d05a944e89401d5504c
w30052974 <wangyifan94@huawei.com>
2024-09-11 17:14:08 +0800

    nnrt 5.0 beta1 黄蓝同步
    Signed-off-by: w30052974 <wangyifan94@huawei.com>

--- frameworks/native/neural_network_core/backend_registrar.cpp
+++ frameworks/native/neural_network_core/backend_registrar.cpp
@@ -16,15 +16,14 @@
 #include "backend_registrar.h"
 
 #include "common/log.h"
-#include "backend_manager.h"
 #include "interfaces/kits/c/neural_network_runtime/neural_network_runtime_type.h"
 
 namespace OHOS {
 namespace NeuralNetworkRuntime {
-BackendRegistrar::BackendRegistrar(const CreateBackend creator)
+BackendRegistrar::BackendRegistrar(const std::string& backendName, const CreateBackend creator)
 {
     auto& backendManager = BackendManager::GetInstance();
-    OH_NN_ReturnCode ret = backendManager.RegisterBackend(creator);
+    OH_NN_ReturnCode ret = backendManager.RegisterBackend(backend, creator);
     if (ret != OH_NN_SUCCESS) {
         LOGW("[BackendRegistrar] Register backend failed. ErrorCode=%{public}d", ret);
     }
